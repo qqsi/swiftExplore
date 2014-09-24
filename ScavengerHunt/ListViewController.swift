c5536699a2d0fcf95b1df187f8ef3abea86216db
qqsi <taog@andrew.cmu.edu>
2014-09-24 18:27:16 -0400

    initial table view

---
+++ ScavengerHunt/ListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ListViewController.swift
+//  ScavengerHunt
+//
+//  Created by Cambi Guo on 9/24/14.
+//  Copyright (c) 2014 Guo Tao. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController : UITableViewController {
+    
+    var itemsList = ["Cat", "Bird", "Brick"]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+            let cell = tableView.dequeueReusableCellWithIdentifier("ListViewCell") as UITableViewCell
+            cell.textLabel!.text = itemsList[indexPath.row]
+        
+//            if let theLabel = cell.textLabel {
+//                theLabel.text = itemsList[indexPath.row]
+//            }
+//        
+//            cell.textLabel?.text = itemsList[indexPath.row]
+        
+            return cell
+    }
+}
+
